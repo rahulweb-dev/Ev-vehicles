1afdddbea9d3ea82cd8cb64582f3a5feb2c46baa
rahulweb-dev <rahulwebdeveloper12@gmail.com>
2026-06-03 09:42:26 +0530

    updated some changes

--- app/cars/page.js
+++ app/cars/page.js
@@ -3,7 +3,6 @@ import Link from "next/link";
 import { Star, BatteryCharging, Gauge, Zap, ChevronRight } from "lucide-react";
 import { AdBannerHorizontal } from "@/components/ads/AdBanner";
 import ArticlesFeed from "@/components/skeletons/ArticlesFeed";
-import { electricCars } from "@/data/vehiclesData";
 import { SITE_URL } from "../layout";
 
 export const revalidate = 60;
