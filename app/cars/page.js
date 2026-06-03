6423a5278b3945576f5e2823e5932ca54c555a57
rahulweb-dev <rahulwebdeveloper12@gmail.com>
2026-06-03 09:45:49 +0530

    updtade ednde w

--- app/cars/page.js
+++ app/cars/page.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { Star, BatteryCharging, Gauge, Zap, ChevronRight } from "lucide-react";
 import { AdBannerHorizontal } from "@/components/ads/AdBanner";
 import ArticlesFeed from "@/components/skeletons/ArticlesFeed";
+import { electricCars } from "@/data/vehiclesData";
 import { SITE_URL } from "../layout";
 
 export const revalidate = 60;
